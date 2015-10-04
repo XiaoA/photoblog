c91e9e12463ecce09e53adb69cd3293da4dc66b8
XiaoA <abuckingham99@gmail.com>
2015-10-04 07:22:07 -0700

    Add Post views and actions

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -1,6 +1,26 @@
 class PostsController < ApplicationController
 
   def index
-    @posts=Post.all
+    @posts = Post.all
   end
+
+  def show
+  end
+  
+  def new
+    @post = Post.new
+  end
+
+  def edit
+  end
+
+  def create
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+  
 end
