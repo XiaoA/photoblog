e793eed1112efdcb78fef038ed10ba33e9e87d03
XiaoA <abuckingham99@gmail.com>
2015-10-09 16:30:19 -0700

    Prevent assets from initializing on precompile

--- config/application.rb
+++ config/application.rb
@@ -10,7 +10,6 @@ module Photoblog
   class Application < Rails::Application
 
     config.assets.initialize_on_precompile = false
-
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
